8f28dd4582edc18edff434fd695562a4f402d3fb
Aironys Garrido <aironys1@hotmail.com>
2022-03-23 01:15:04 -0300

    Query-Selector

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('titulo').innerHTML = 'Minha Receita';
+
+document.querySelector('#titulo').innerHTML = "Receita de Domingo";
+
+document.querySelector('.subtitulo').innerHTML = "Meus Igredientes";
+
+let lista = document.querySelectorAll('li');
+
+document.querySelector('body').style.backgroundColor ='blue';
+
+document.querySelector('li').style.backgroundColor = 'white';
+document.querySelector('li.l1').style.color = 'red';
